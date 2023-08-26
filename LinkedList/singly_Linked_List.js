f6890b9c56c083373f654a8523d4af39dae057a6
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-08-26 17:16:05 +0600

    add(), insertAt(), removeFrom() methods are added in singly linkedlist

--- LinkedList/singly_Linked_List.js
+++ LinkedList/singly_Linked_List.js
@@ -19,10 +19,8 @@ class LinkedList {
       this.head = node;
     } else {
       current = this.head;
-      console.log(current.element, "1st");
       while (current.next) {
         current = current.next;
-        console.log(current.element, "2nd");
       }
       current.next = node;
     }
@@ -30,12 +28,66 @@ class LinkedList {
   }
 
   //.....insertAt(element, index) .....//
+  insertAt(element, index) {
+    if (index < 0 || index > this.size) {
+      return console.log("Please enter a valid index");
+    } else {
+      let node = new Node(element);
+      let current, previous;
+      current = this.head;
+
+      if (index === 0) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        let iterator = 0;
+        while (iterator < index) {
+          previous = current;
+          current = current.next;
+          iterator++;
+        }
+        node.next = current;
+        previous.next = node;
+      }
+      this.size++;
+    }
+  }
+  //.....removeFrom(index) .....//
+  removeFrom(index) {
+    if (index < 0 || index >= this.size) {
+      return console.log("Please Enter a valid index");
+    } else {
+      let current,
+        previous,
+        iterator = 0;
+      current = this.head;
+      previous = current;
+
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        while (iterator < index) {
+          previous = current;
+          current = current.next;
+          iterator++;
+        }
+        console.log(previous);
+        console.log(current);
+        previous.next = current.next;
+      }
+      this.size--;
+    }
+  }
 }
 
 const list = new LinkedList();
 list.add(10);
 list.add(20);
 list.add(30);
-list.add(40);
 list.add(50);
+
+list.insertAt(60, 3);
+
+list.removeFrom(2);
+
 console.log(list);
