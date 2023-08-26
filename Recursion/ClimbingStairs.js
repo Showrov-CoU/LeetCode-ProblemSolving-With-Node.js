87f2c1e2b09a86cb98a9a672ebaae36944598f23
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-08-26 18:24:19 +0600

    Climbing Stairs with recursion

---
+++ Recursion/ClimbingStairs.js
@@ -0,0 +1,12 @@
+const mem = {};
+const climbStairs = (n) => {
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+  if (mem[n]) return mem[n];
+  mem[n] = climbStairs(n - 1) + climbStairs(n - 2);
+  return mem[n];
+};
+process.stdin.on("data", (data) => {
+  let result = climbStairs(data.toString().trim());
+  console.log(result);
+});
