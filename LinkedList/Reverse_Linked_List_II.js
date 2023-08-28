21a48aadfe0a5d69c6c97f0f19be371fd6587912
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-08-28 08:02:46 +0600

    Reverse linked list between left to right

--- LinkedList/Reverse_Linked_List_II.js
+++ LinkedList/Reverse_Linked_List_II.js
@@ -97,13 +97,3 @@ const getAllInput = async () => {
 };
 
 getAllInput();
-
-// let list = new LinkedList();
-// list.add(0);
-// list.add(1);
-// list.add(2);
-// list.add(3);
-// list.add(4);
-// list.add(5);
-
-// reverseBetween(list, 1, 5);
